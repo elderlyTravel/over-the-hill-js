21122800adfcdec6f8e40c7e6b8749a5633776e4
olivia-leach <olivia.i.m.leach@gmail.com>
2016-05-26 15:18:25 -0400

    Add listener for "show me flights to"
    Confirms location on user interface
    Displays yes and no buttons

--- assets/scripts/app/annyang.js
+++ assets/scripts/app/annyang.js
@@ -1,15 +1,32 @@
+  'use strict';
+
   if (annyang) {
 
       // Set annyang in debug mode
       annyang.debug();
 
+      let print = function(tag) {
+        console.log('You want to go to ' + tag + '?');
+        $('header')
+        .append('<p class="question">You want to go to <span>' + tag + '</span> is that right?</p><button id="location" class="btn btn-primary btn-lg">Yes</button><button id="cancel" class="btn btn-danger btn-lg">No</button>');
+      };
+
       // Let's define a command.
       var commands = {
-          'hello': function() { console.log('Hello world!'); }
+          'hello': function() { console.log('Hello world!'); },
+
+          // annyang will capture anything after a splat (*) and pass it to the function.
+          'show me flights to *tag': print
       };
 
       // Add our commands to annyang
       annyang.addCommands(commands);
+
+      // annyang.addCallback('resultMatch', function(userSaid, commandText, phrases) {
+      //   console.log(userSaid); // sample output: 'hello'
+      //   console.log(commandText); // sample output: 'hello (there)'
+      //   console.log(phrases); // sample output: ['hello', 'halo', 'yellow', 'polo', 'hello kitty']
+      // });
 
       // Tell KITT to communicate with annyang
       SpeechKITT.annyang();
